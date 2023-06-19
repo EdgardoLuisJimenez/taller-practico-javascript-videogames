d2c03568d1d37ce8602f52fe18a27c7caeec7280
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-06-19 12:29:15 -0500

    Object playerPosition

--- game.js
+++ game.js
@@ -7,6 +7,11 @@ const game = canvas.getContext("2d");
 let canvasSize;
 let elementsSize;
 
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
+
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
 
@@ -31,10 +36,9 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  const map = maps[1];
+  const map = maps[0];
   const mapRows = map.trim().split("\n");
   const mapRowCols = mapRows.map((row) => row.trim().split(""));
-  // console.log(mapRows ,mapRowCols);
 
   mapRowCols.forEach((row, rowIndex) => {
     row.forEach((col, colIndex) => {
@@ -42,6 +46,34 @@ function startGame() {
       const positionX = elementsSize * (colIndex + 1);
       const positionY = elementsSize * (rowIndex + 1);
       game.fillText(emoji, positionX, positionY);
+
+      if (col == "O") {
+        playerPosition.x = positionX;
+        playerPosition.y = positionY;
+        console.log({ playerPosition });
+      }
     });
   });
+
+  movePlayer();
 }
+
+function movePlayer() {
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+function move(key) {
+  console.log(`Pressed: ${key}`);
+}
+
+const moveByKey = (event) => {
+  if (event.key == "ArrowUp") {
+    move("up");
+    playerPosition.y -= elementsSize;
+    startGame();
+  } else if (event.key == "ArrowLeft") move("left");
+  else if (event.key == "ArrowRight") move("right");
+  else if (event.key == "ArrowDown") move("down");
+};
+
+window.addEventListener("keydown", moveByKey);
