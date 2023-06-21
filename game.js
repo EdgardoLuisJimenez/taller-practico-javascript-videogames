21ca4e4bec2f972f51aac541287588ce98c982b6
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-06-21 11:21:17 -0500

    GameWin, levelUp

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ const btnDown = document.querySelector("#down");
 
 let canvasSize;
 let elementsSize;
+let level = 0;
 
 const playerPosition = {
   x: undefined,
@@ -51,7 +52,13 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  const map = maps[0];
+  const map = maps[level];
+
+  if (!map) {
+    gameWin();
+    return;
+  }
+
   const mapRows = map.trim().split("\n");
   const mapRowCols = mapRows.map((row) => row.trim().split(""));
 
@@ -68,11 +75,11 @@ function startGame() {
       } else if (col == "I") {
         gitftPosition.x = positionX;
         gitftPosition.y = positionY;
-      } else if (col == 'X') {
+      } else if (col == "X") {
         enemyPositions.push({
           x: positionX,
           y: positionY,
-        })
+        });
       }
     });
   });
@@ -82,26 +89,36 @@ function startGame() {
 
 function movePlayer() {
   const giftCollissionX =
-    (playerPosition.x.toFixed(3)) == (gitftPosition.x.toFixed(3));
+    playerPosition.x.toFixed(3) == gitftPosition.x.toFixed(3);
   const giftCollissionY =
-    (playerPosition.y.toFixed(3)) == (gitftPosition.y.toFixed(3));
+    playerPosition.y.toFixed(3) == gitftPosition.y.toFixed(3);
   const giftCollission = giftCollissionX && giftCollissionY;
 
   if (giftCollission) {
-    console.log("Subistes de nivel!!");
+    levelWin();
   }
 
-  const enemyCollision = enemyPositions.find(enemy => {
+  const enemyCollision = enemyPositions.find((enemy) => {
     const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
     const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
     return enemyCollisionX && enemyCollisionY;
   });
 
   if (enemyCollision) {
-    console.log('Chocaste contra un enemigo!!');
+    console.log("Chocaste contra un enemigo!!");
   }
 
   game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+function levelWin() {
+  console.log("Subistes de nivel");
+  level++;
+  startGame();
+}
+
+function gameWin() {
+  console.log("Terminastes el juego!!");
 }
 
 btnUp.addEventListener("click", moveUp);
