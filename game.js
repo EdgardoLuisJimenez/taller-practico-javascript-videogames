964386607d65161c7e8f0a829932b6c9355c8cf1
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-06-24 22:27:53 -0500

    Restructuring of the archives

--- game.js
+++ game.js
@@ -1,6 +1,70 @@
 /**
  * @type {HTMLCanvasElement};
  */
+document.addEventListener("DOMContentLoaded", () => {
+  let gameContainer = document.createElement("div");
+  gameContainer.className = "game-container";
+
+  let canvasDiv = document.createElement("canvas");
+  canvasDiv.id = "game";
+  gameContainer.appendChild(canvasDiv);
+
+  let btnContainer = document.createElement("div");
+  btnContainer.className = "btns";
+  gameContainer.appendChild(btnContainer);
+
+  let upBtn = document.createElement("button");
+  upBtn.id = "up";
+  upBtn.innerText = "Arriba";
+  btnContainer.appendChild(upBtn);
+
+  let leftBtn = document.createElement("button");
+  leftBtn.id = "left";
+  leftBtn.innerText = "Izquierda";
+  btnContainer.appendChild(leftBtn);
+
+  let rightBtn = document.createElement("button");
+  rightBtn.id = "right";
+  rightBtn.innerText = "Derecha";
+  btnContainer.appendChild(rightBtn);
+
+  let downBtn = document.createElement("button");
+  downBtn.id = "down";
+  downBtn.innerText = "abajo";
+  btnContainer.appendChild(downBtn);
+
+  let messagesContainer = document.createElement("div");
+  messagesContainer.className = "messages";
+  gameContainer.appendChild(messagesContainer);
+
+  let livesP = document.createElement("p");
+  livesP.innerHTML = 'Vidas: <span id="lives"></span>';
+  messagesContainer.appendChild(livesP);
+
+  let timeP = document.createElement("p");
+  timeP.innerHTML = 'Tiempo: ⏰<span id="time"></span>';
+  messagesContainer.appendChild(timeP);
+
+  let recordP = document.createElement("p");
+  recordP.innerHTML = 'Record: 🏁<span id="record"></span>';
+  messagesContainer.appendChild(recordP);
+
+  let resultP = document.createElement("p");
+  resultP.id = "result";
+  messagesContainer.appendChild(resultP);
+
+  let body = document.querySelector("body");
+  body.appendChild(gameContainer);
+
+  let mapsScript = document.createElement("script");
+  mapsScript.src = "./maps.js";
+  body.appendChild(mapsScript);
+
+
+
+
+  
+
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 const btnUp = document.querySelector("#up");
@@ -181,6 +245,9 @@ function levelFail() {
     level = 0;
     lives = 3;
     timeStart = undefined;
+
+    // Llamada a la funcion
+    showGameOver();
   }
 
   playerPosition.x = undefined;
@@ -285,3 +352,33 @@ function moveDown(key) {
     startGame();
   }
 }
+
+function showGameOver() {
+  // Game Over
+  let gameOver = document.createElement("div");
+  gameOver.id = "game-over";
+
+  let gameOverText = document.createElement("p");
+  gameOverText.textContent = "Game over";
+  gameOver.appendChild(gameOverText);
+
+  let playAgainText = document.createElement("p");
+  playAgainText.textContent = "Play Again?";
+  gameOver.appendChild(playAgainText);
+
+  let buttonContainer = document.createElement("div");
+  buttonContainer.id = "game-over--button";
+
+  let yesButton = document.createElement("button");
+  yesButton.textContent = "Yes";
+  buttonContainer.appendChild(yesButton);
+
+  let noButton = document.createElement("button");
+  noButton.textContent = "No";
+  buttonContainer.appendChild(noButton);
+
+  gameOver.appendChild(buttonContainer);
+
+  document.body.appendChild(gameOver);
+}
+});
